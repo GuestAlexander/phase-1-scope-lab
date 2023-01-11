7d0ca76a6102bb862b72f3888fe9bd6936a935be
Alexander <alexanderguest@me.com>
2023-01-11 10:52:13 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// var bestCustomer = "not bob";
+function setBestCustomer() {
+    if (customerName === "bob") {
+        bestCustomer = "bob";
+    }
+    else {
+        bestCustomer = "not bob";
+    }
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "steve"
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "maybe bob"
+}
