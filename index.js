f083274f0b0626ccf672c409806a42e4f7cae173
Alexander <alexanderguest@me.com>
2023-01-11 08:30:18 -0500

    commit tree d5a242b72f8daaa23ff24fcfe475bb440a6bf657

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer = "not bob";
+function bestCustomer() {
+    if (customerName === "bob") {
+        bestCustomer = "bob";
+    }
+    else {
+        bestCustomer = "not bob";
+    }
+}
